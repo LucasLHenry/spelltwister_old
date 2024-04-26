a9509bc6315b2b05319a918af6e7cc9c9ecf6edb
Lucas Henry <lucasleroyhenry@gmail.com>
2024-04-26 13:41:51 -0700

    changed end of cycle led on time

--- src/objects/module.h
+++ src/objects/module.h
@@ -14,7 +14,7 @@
 #define HZPHASOR 91183 //phasor value for 1 hz.
 
 #define TRIG_LENGTH_MS 0.25
-#define TRIG_LED_LENGTH_MS 25
+#define TRIG_LED_LENGTH_MS 75
 // ms * 1s/1000ms * 48000 updates/s
 constexpr uint64_t trig_length_in_updates = static_cast<uint64_t>(TRIG_LENGTH_MS * 48);
 constexpr uint64_t trig_led_length_in_updates = static_cast<uint64_t>(TRIG_LED_LENGTH_MS * 48);
